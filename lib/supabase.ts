2c40e09735568d36913f018f886360e0e06ca836
juliocesarblancomedina08-tech <juliocesarblancomedina08@gmail.com>
2026-09-17 14:28:58 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,28 +1,29 @@
-import  {  createClient  }  from  "@supabase/supabase-js" ;
+import { createClient } from "@supabase/supabase-js";
 
-const  supabaseUrl  =  process . env . NEXT_PUBLIC_SUPABASE_URL ;
-const  superbasePublishableKey  =
-proceso . env . NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabasePublishableKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-si  ( ! supabaseUrl )  {
-lanzar  nuevo  Error (
-"Falta NEXT_PUBLIC_SUPABASE_URL en las variables de entorno".
-) ;
+if (!supabaseUrl) {
+  throw new Error(
+    "Falta NEXT_PUBLIC_SUPABASE_URL en las variables de entorno."
+  );
 }
 
-si  ( ! supabasePublishableKey )  {
-lanzar  nuevo  Error (
-"Falta NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY en las variables de entorno".
-) ;
+if (!supabasePublishableKey) {
+  throw new Error(
+    "Falta NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY en las variables de entorno."
+  );
 }
 
-export  const  supabase  =  createClient (
-supabaseUrl ,
-supabasePublishableKey ,
-{
-autenticación : {
-persistSession : verdadero ,
-autoRefreshToken : verdadero ,
-detectSessionInUrl : verdadero ,
-} ,
-}
+export const supabase = createClient(
+  supabaseUrl,
+  supabasePublishableKey,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  }
+);
