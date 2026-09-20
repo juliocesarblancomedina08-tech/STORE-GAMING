9b4ae075c7dff20bfb8ec5f46cfe6e88f9e9b37b
juliocesarblancomedina08-tech <juliocesarblancomedina08@gmail.com>
2026-09-20 07:56:44 -0400

    Update fc-mobile-id.ts

--- lib/games/fc-mobile-id.ts
+++ lib/games/fc-mobile-id.ts
@@ -22,225 +22,141 @@ export const FC_MOBILE_ID = {
   offers: [
     {
       id: "fc-40",
-
       supplierOfferId: "40_fc_points",
-
       name: "40 FC Points",
-
       display: "40 FC",
-
       price: 0.51,
-
       supplierPrice: 0.3526,
-
       icon: "⚽",
     },
 
     {
       id: "fc-100",
-
       supplierOfferId: "100_fc_points",
-
       name: "100 FC Points",
-
       display: "100 FC",
-
       price: 1.02,
-
       supplierPrice: 0.8665,
-
       icon: "⚽",
     },
 
     {
       id: "fc-520",
-
       supplierOfferId: "520_fc_points",
-
       name: "520 FC Points",
-
       display: "520 FC",
-
       price: 4.41,
-
       supplierPrice: 4.2617,
-
       icon: "⚽",
     },
 
     {
       id: "fc-1070",
-
       supplierOfferId: "1070_fc_points",
-
       name: "1070 FC Puntos",
-
       display: "1070 FC",
-
       price: 8.72,
-
       supplierPrice: 8.5738,
-
       icon: "⚽",
     },
 
     {
       id: "fc-2200",
-
       supplierOfferId: "2200_fc_points",
-
       name: "2200 FC Points",
-
       display: "2200 FC",
-
       price: 17.89,
-
       supplierPrice: 17.7421,
-
       icon: "⚽",
     },
 
     {
       id: "fc-5750",
-
       supplierOfferId: "5750_fc_points",
-
       name: "5750 FC Points",
-
       display: "5750 FC",
-
       price: 43.24,
-
       supplierPrice: 43.0908,
-
       icon: "⚽",
     },
 
     {
       id: "fc-12000",
-
       supplierOfferId: "12000_fc_points",
-
       name: "12000 FC Points",
-
       display: "12000 FC",
-
       price: 86.39,
-
       supplierPrice: 86.242,
-
       icon: "⚽",
     },
 
     {
       id: "fc-39-silver",
-
       supplierOfferId: "39_silver",
-
       name: "39 Silver",
-
       display: "39 Plata",
-
       price: 0.51,
-
       supplierPrice: 0.3526,
-
       icon: "🪙",
     },
 
     {
       id: "fc-99-silver",
-
       supplierOfferId: "99_silver",
-
       name: "99 Silver",
-
       display: "99 Plata",
-
       price: 1.02,
-
       supplierPrice: 0.8665,
-
       icon: "🪙",
     },
 
     {
       id: "fc-499-silver",
-
       supplierOfferId: "499_silver",
-
       name: "499 Silver",
-
       display: "499 Plata",
-
       price: 4.41,
-
       supplierPrice: 4.2617,
-
       icon: "🪙",
     },
 
     {
       id: "fc-999-plata",
-
       supplierOfferId: "999_plata",
-
       name: "999 Plata",
-
       display: "999 Plata",
-
       price: 8.72,
-
       supplierPrice: 8.5738,
-
       icon: "🪙",
     },
 
     {
       id: "fc-1999-plata",
-
       supplierOfferId: "1999_plata",
-
       name: "1999 Plata",
-
       display: "1999 Plata",
-
       price: 17.89,
-
       supplierPrice: 17.7421,
-
       icon: "🪙",
     },
 
     {
       id: "fc-4999-plata",
-
       supplierOfferId: "4999_plata",
-
       name: "4999 Plata",
-
       display: "4999 Plata",
-
       price: 43.24,
-
       supplierPrice: 43.0908,
-
       icon: "🪙",
     },
 
     {
       id: "fc-9999-plata",
-
       supplierOfferId: "9999_plata",
-
       name: "9999 Plata",
-
       display: "9999 Plata",
-
       price: 86.39,
-
       supplierPrice: 86.242,
-
       icon: "🪙",
     },
   ],
